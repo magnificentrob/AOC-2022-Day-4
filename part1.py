5bc462ee98b04d82cb15414561b0414fa12a8e71
magnificentrob <31416219+magnificentrob@users.noreply.github.com>
2022-12-05 17:15:11 -0500

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,22 @@
+import csv
+pair1=[]
+pair2=[]
+with open('input.txt') as file:
+	csv_reader = csv.reader(file, delimiter = ',')
+	for row in csv_reader:
+		row[0] = row[0].split('-')
+		row[1] = row[1].split('-')
+		pair1.append(row[0])
+		pair2.append(row[1])
+
+for i in range(len(pair1)):
+	pair1[i] = list(range(int(pair1[i][0]),int(pair1[i][1])+1))
+	pair2[i] = list(range(int(pair2[i][0]),int(pair2[i][1])+1))
+
+count =0
+for i in range(len(pair1)):
+	if (set(pair1[i]).issubset(set(pair2[i]))):
+		count+=1
+	elif(set(pair2[i]).issubset(set(pair1[i]))):
+		count+=1
+print(count)
